31fb286bf1bc82111a9c5e81bf0fbb1325ff4687
yukitaira000@gmail.com <yukitaira000@gmail.com>
2022-03-28 15:34:09 +0900

    toggleというメソッド名を変更、querySelectoeAllの詳細度を修正

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const openBtn = document.querySelectorAll('.accordion > li > a > span.btn');
+const openBtn = document.querySelectorAll('.btn');
 // btnクラスをHTMLより取得
-const parent = document.querySelectorAll('.accordion > li > a.content');
+const parent = document.querySelectorAll('.content');
 // contentクラスをHTMLより取得
 
 
 for(let i = 0; i < openBtn.length; i ++){
-  const toggle = function(){
+  const menuAction = function(){
     this.classList.toggle('active');
     // 開閉ボタンを作動させるために、開閉ボタンにもactiveクラスをつけたり外したりしています
     parent[i].nextElementSibling.classList.toggle('active');
     // 今実行しているopenBtnのindex番号と同じparentのindex番号の次のものにactiveクラスをつける、またはついていれば削除する
   }
-  openBtn[i].addEventListener('click', toggle);
+  openBtn[i].addEventListener('click', menuAction);
   // 開閉ボタンをクリックしたときにtoggleと定義した関数を呼び出し実行する。
 }
